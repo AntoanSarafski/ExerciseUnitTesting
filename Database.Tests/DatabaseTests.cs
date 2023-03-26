ddb7c08afc7777cdec5488ed166b5820dbebcc70
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-26 14:58:32 +0300

    Commit with testing AddMethod. Checking the number of additional elements and Is the added element equal to our element.

--- Database.Tests/DatabaseTests.cs
+++ Database.Tests/DatabaseTests.cs
@@ -18,5 +18,16 @@ namespace Database.Tests
         {
             _database = null;
         }
+
+        [Test]  
+        public void AddMethodTest() 
+        {
+            _database.Add(1488);
+            int[] result = _database.Fetch();
+
+            Assert.IsTrue(1 == _database.Count);
+            Assert.AreEqual(1488, result[0]);
+            Assert.AreEqual(1, result.Length);
+        }
     }
 }
