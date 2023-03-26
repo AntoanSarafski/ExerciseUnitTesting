dea1f04f87cd9d70001c8a87a9a9c71bf3d562fa
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-26 14:49:49 +0300

    Commit with SetUp with creation _database before every test and TearDown with cleaning t he _database after every test.

--- Database.Tests/DatabaseTests.cs
+++ Database.Tests/DatabaseTests.cs
@@ -5,6 +5,18 @@ namespace Database.Tests
     [TestFixture]
     public class DatabaseTests
     {
+        private Database _database;
 
+        [SetUp]
+        public void Setup()
+        {
+            _database = new Database();
+        }
+
+        [TearDown]  
+        public void TearDown()
+        {
+            _database = null;
+        }
     }
 }
